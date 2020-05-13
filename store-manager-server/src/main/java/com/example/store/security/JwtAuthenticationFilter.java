17751f381bb6c1581091a096990962b4f53bd84c
nguyensyan <syan224@gmail.com>
2020-05-13 12:46:06 +0700

    update postman

--- store-manager-server/src/main/java/com/example/store/security/JwtAuthenticationFilter.java
+++ store-manager-server/src/main/java/com/example/store/security/JwtAuthenticationFilter.java
@@ -57,6 +57,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private String getJwtFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            logger.debug(bearerToken);
             return bearerToken.substring(7, bearerToken.length());
         }
         return null;
