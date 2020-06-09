a5aa31f4ca0b3d34bb9bc09f15909280454aa946
Trung Dung Vu <trungdungvu172@gmail.com>
2020-06-09 09:43:21 +0700

    supplier show

--- store-manager-client/src/_services/index.js
+++ store-manager-client/src/_services/index.js
@@ -1,1 +1,5 @@
-export * from "./user.service"
+export * from "./user.service";
+export * from "./employee.service";
+export * from "./customer.service";
+export * from "./supplier.service";
+export * from "./validate.service";
